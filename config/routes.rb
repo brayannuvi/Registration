a3763963abdee9a67b3a49dde34c5f359c56328e
brayannuvi <106778840+brayannuvi@users.noreply.github.com>
2024-01-29 09:59:35 -0500

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "users#new"
   
   get '/users/get_all_users', to: 'users#get_all_users'
 
